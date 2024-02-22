8ec36bf21af0e07b507db277019dbd1444a7c017
sesztaklorant <lorant.sesztak@stud.uni-corvinus.hu>
2024-02-22 10:47:02 +0100

    Kesz Fibonacci project hozzáadása

--- Fibonacci/Form1.Designer.cs
+++ Fibonacci/Form1.Designer.cs
@@ -28,18 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            szurkenegyzet = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)szurkenegyzet).BeginInit();
             SuspendLayout();
+            // 
+            // szurkenegyzet
+            // 
+            szurkenegyzet.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            szurkenegyzet.Location = new Point(364, 37);
+            szurkenegyzet.Name = "szurkenegyzet";
+            szurkenegyzet.RowTemplate.Height = 25;
+            szurkenegyzet.Size = new Size(385, 384);
+            szurkenegyzet.TabIndex = 0;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(szurkenegyzet);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)szurkenegyzet).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView szurkenegyzet;
     }
 }
--- Fibonacci/Form1.cs
+++ Fibonacci/Form1.cs
@@ -5,22 +5,36 @@ namespace Fibonacci
         public Form1()
         {
             InitializeComponent();
+
+            List<Elem> listam = new List<Elem>();
+
             for (int i = 0; i < 10; i++)
             {
                 Button b = new Button();
                 b.Top = i * 30;
                 b.Text = Fibonacci(i).ToString();
                 Controls.Add(b);
+
+                Elem ujSor = new Elem();
+
+                ujSor.Sorszam = i + 1;
+                ujSor.Ertek = Fibonacci(i);
+
+                listam.Add(ujSor);
+
             }
+
+            szurkenegyzet.DataSource = listam;
+
         }
 
         int Fibonacci(int n)
         {
             if (n == 0) { return 0; }
-            if (n == 1) {  return 1; }
-            return Fibonacci(n-1) + Fibonacci(n-2);
+            if (n == 1) { return 1; }
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
         }
 
-        
+
     }
 }
