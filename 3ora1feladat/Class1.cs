418f8b2ffc061cff7f3ace2c357bb43e972848e0
sesztaklorant <lorant.sesztak@stud.uni-corvinus.hu>
2024-02-29 10:09:02 +0100

    elso feladat keszen

---
+++ 3ora1feladat/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ora1feladat
+{
+    internal class VillogoGomb : Button
+    {
+        public VillogoGomb()
+        {
+            //ctor tab tab gyors
+            MouseEnter += OnMouseEnter;
+            MouseLeave += OnMouseLeave;
+        }
+
+        private void OnMouseLeave(object? sender, EventArgs e)
+        {
+            BackColor = SystemColors.ButtonFace;
+        }
+
+        private void OnMouseEnter(object? sender, EventArgs e)
+        {
+            BackColor = Color.Fuchsia;
+        }
+    }
+}
---
+++ 3ora1feladat/Form1.cs
@@ -0,0 +1,35 @@
+namespace _3ora1feladat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+
+                    VillogoGomb uj = new VillogoGomb();
+                    uj.Width = 100;
+                    uj.Height = 50;
+
+                    uj.Left = i * 110;
+                    uj.Top = j * 55;
+
+                    uj.Text = (i * j).ToString();
+
+                    //uj.Left = ClientRectangle.Width / 2 - 50;
+                    //uj.Top = ClientRectangle.Height / 2 - 25;
+
+                    Controls.Add(uj);
+                }
+            }
+            
+        }
+    }
+}
