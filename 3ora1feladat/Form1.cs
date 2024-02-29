b6a565fab6b826b04de9e5674ea70e598eac18ed
sesztaklorant <lorant.sesztak@stud.uni-corvinus.hu>
2024-02-29 10:43:18 +0100

    negyedik feladat

--- 3ora1feladat/Form1.cs
+++ 3ora1feladat/Form1.cs
@@ -2,14 +2,38 @@ namespace _3ora1feladat
 {
     public partial class Form1 : Form
     {
+        Random rng = new Random();
+
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int width = rng.Next(20, 51);
+                int height = rng.Next(20, 51);
+                int left = rng.Next(this.ClientSize.Width - width);
+                int top = rng.Next(this.ClientSize.Height - height);
+                int r = rng.Next(256);
+                int g = rng.Next(256);
+                int b = rng.Next(256);
+                Button gomb = new Button();
+                gomb.Width = width;
+                gomb.Height = height;
+                gomb.Left = left;
+                gomb.Top = top;
+                gomb.BackColor = Color.FromArgb(r, g, b);
+                this.Controls.Add(gomb);
+            }
         }
+        /*public Form1()
+        {
+            InitializeComponent();
+        }*/
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            for (int j = 0; j < 10; j++)
+            /*for (int j = 0; j < 10; j++)
             {
                 for (int i = 0; i < 10; i++)
                 {
@@ -28,8 +52,8 @@ namespace _3ora1feladat
 
                     Controls.Add(uj);
                 }
-            }
-            
+            }*/
+
         }
     }
 }
