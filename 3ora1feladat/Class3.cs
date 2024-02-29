632e408440faaf4415d1fd76465a6c89b0a2ab30
sesztaklorant <lorant.sesztak@stud.uni-corvinus.hu>
2024-02-29 10:28:29 +0100

    harmadik feladat megoldas

---
+++ 3ora1feladat/Class3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ora1feladat
+{
+    internal class SzamoloGomb : Button
+    {
+        int szam;
+        public SzamoloGomb() 
+        {
+            Width = 20;
+            Height = 20;
+            szam = 1;
+            Click += SzamoloGomb_Click;
+            Text = szam.ToString();
+        }
+
+        private void SzamoloGomb_Click(object? sender, EventArgs e)
+        {
+            szam++;
+            if (szam == 6)
+            {
+                szam = 1;
+            }
+            Text = szam.ToString();
+        }
+    }
+}
--- 3ora1feladat/Form1.cs
+++ 3ora1feladat/Form1.cs
@@ -14,14 +14,14 @@ namespace _3ora1feladat
                 for (int i = 0; i < 10; i++)
                 {
 
-                    SzinezoGomb uj = new SzinezoGomb();
+                    SzamoloGomb uj = new SzamoloGomb();
                     uj.Width = 100;
                     uj.Height = 50;
 
                     uj.Left = i * 110;
                     uj.Top = j * 55;
 
-                    uj.Text = (i * j).ToString();
+                    //uj.Text = (i * j).ToString();
 
                     //uj.Left = ClientRectangle.Width / 2 - 50;
                     //uj.Top = ClientRectangle.Height / 2 - 25;
