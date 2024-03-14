6cc8f6d56dc80751c3e56c8e966580f7e7e74ac5
sesztaklorant <lorant.sesztak@stud.uni-corvinus.hu>
2024-03-14 11:00:36 +0100

    Hajos teszt orai feltoltve

---
+++ Hajos teszt/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Hajos_teszt
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dataGridView1 = new DataGridView();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            valaszGomb1 = new ValaszGomb();
+            valaszGomb2 = new ValaszGomb();
+            valaszGomb3 = new ValaszGomb();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(66, 266);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(666, 150);
+            dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(68, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(441, 40);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(291, 201);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // valaszGomb1
+            // 
+            valaszGomb1.BackColor = Color.LightGray;
+            valaszGomb1.BorderStyle = BorderStyle.None;
+            valaszGomb1.Location = new Point(76, 62);
+            valaszGomb1.Multiline = true;
+            valaszGomb1.Name = "valaszGomb1";
+            valaszGomb1.ReadOnly = true;
+            valaszGomb1.Size = new Size(348, 33);
+            valaszGomb1.TabIndex = 3;
+            // 
+            // valaszGomb2
+            // 
+            valaszGomb2.BackColor = Color.LightGray;
+            valaszGomb2.BorderStyle = BorderStyle.None;
+            valaszGomb2.Location = new Point(76, 113);
+            valaszGomb2.Multiline = true;
+            valaszGomb2.Name = "valaszGomb2";
+            valaszGomb2.ReadOnly = true;
+            valaszGomb2.Size = new Size(348, 40);
+            valaszGomb2.TabIndex = 4;
+            // 
+            // valaszGomb3
+            // 
+            valaszGomb3.BackColor = Color.LightGray;
+            valaszGomb3.BorderStyle = BorderStyle.None;
+            valaszGomb3.Location = new Point(75, 174);
+            valaszGomb3.Multiline = true;
+            valaszGomb3.Name = "valaszGomb3";
+            valaszGomb3.ReadOnly = true;
+            valaszGomb3.Size = new Size(349, 42);
+            valaszGomb3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(valaszGomb3);
+            Controls.Add(valaszGomb2);
+            Controls.Add(valaszGomb1);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private ValaszGomb valaszGomb1;
+        private ValaszGomb valaszGomb2;
+        private ValaszGomb valaszGomb3;
+    }
+}
