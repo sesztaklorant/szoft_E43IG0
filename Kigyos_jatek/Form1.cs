6db4379391befff219686d75a048f4d65942075f
sesztaklorant <lorant.sesztak@stud.uni-corvinus.hu>
2024-03-21 10:47:12 +0100

    első része a kígyós feladatnak

---
+++ Kigyos_jatek/Form1.cs
@@ -0,0 +1,106 @@
+namespace Kigyos_jatek
+{
+    public partial class Form1 : Form
+    {
+        int fej_x = 100;
+        int fej_y = 100;
+
+        int hossz = 10;
+
+        int irány_x = 1;
+        int irány_y = 0;
+
+        List<KígyóElem> kígyó = new List<KígyóElem>();
+
+        int lépésszám;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*List<string> list = new List<string>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                //list[i] =
+
+            }
+            foreach (string item in list)
+            {
+
+            }
+            */
+
+            
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lépésszám++;
+
+            //Fejnövesztés
+            fej_x += irány_x * KígyóElem.Méret;
+            fej_y += irány_y * KígyóElem.Méret;
+
+            foreach (KígyóElem item in Controls)
+            {
+                //Ha van már valami ott, ahova az új fejet tenném, vége a játéknak
+                if (item.Top == fej_y && item.Left == fej_x)
+                {
+                    timer1.Enabled = false;
+                    return;
+                }
+            }
+
+            KígyóElem ke = new KígyóElem();
+            ke.Top = fej_y;
+            ke.Left = fej_x;
+
+            //alma mereg
+            kígyó.Add(ke);
+
+            Controls.Add(ke);
+
+            if (kígyó.Count > hossz)
+            {
+                //KígyóElem levágandó = Controls.RemoveAt(0);
+                KígyóElem levágandó = kígyó[0];
+                Controls.RemoveAt(0);
+
+                //
+            }
+
+            if (lépésszám % 2 == 0) ke.BackColor = Color.Yellow;
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                irány_y = -1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Down)
+            {
+                irány_y = 1;
+                irány_x = 0;
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                irány_y = 0;
+                irány_x = -1;
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                irány_y = 0;
+                irány_x = 1;
+            }
+        }
+    }
+}
